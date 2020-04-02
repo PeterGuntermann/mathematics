6ac7fe0daa719388d1821b2daca2dc5b74f9ba7f
Peter Guntermann <pguntermann@andrena.de>
2020-04-02 13:35:08 +0200

    Include Bootstrap via npm

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -34,10 +34,29 @@ module.exports = merge(common, {
         include: Path.resolve(__dirname, '../src'),
         loader: 'babel-loader'
       },
+      // {
+      //   test: /\.s?css$/i,
+      //   use: ['style-loader', 'css-loader?sourceMap=true', 'sass-loader']
+      // },
       {
-        test: /\.s?css$/i,
-        use: ['style-loader', 'css-loader?sourceMap=true', 'sass-loader']
-      }
+        test: /\.(scss)$/,
+        use: [{
+          loader: 'style-loader', // inject CSS to page
+        }, {
+          loader: 'css-loader', // translates CSS into CommonJS modules
+        }, {
+          loader: 'postcss-loader', // Run postcss actions
+          options: {
+            plugins: function () { // postcss plugins, can be exported to postcss.config.js
+              return [
+                require('autoprefixer')
+              ];
+            }
+          }
+        }, {
+          loader: 'sass-loader' // compiles Sass to CSS
+        }]
+      },
     ]
   }
 });
