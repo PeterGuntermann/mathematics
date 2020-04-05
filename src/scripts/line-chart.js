6c88a3bae4220feb544ab132315a61c024aa4e33
Peter Guntermann <pgu@posteo.de>
2020-04-05 17:22:45 +0200

    Remove bezier smoothing

--- src/scripts/line-chart.js
+++ src/scripts/line-chart.js
@@ -7,17 +7,16 @@ export const drawLineChart = () => {
         datasets: [
             {
                 data: [0, 10, 5, 2, 20, 30, 45, 4, 15],
+                lineTension: 0
             },
             {
-                data: [7, 12, 6, 3, 12, 6, 1, 5, 4]
+                data: [7, 12, 6, 3, 12, 6, 1, 5, 4],
+                lineTension: 0
             }]
     };
     const chart = new Chart(ctx, {
         type: 'line',
         data: dummyData,
-        options: {
-            lineTension: 0,
-            clip: { bottom: 10 }
-        }
+        options: {}
     });
 };
