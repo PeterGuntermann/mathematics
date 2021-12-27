965f534e5adc769120efec528b3dca98685b6bbb
Peter Guntermann <pgu@posteo.de>
2021-12-27 22:29:59 +0100

    Start implementing prime decompositions

--- prime-numbers/src/app/app.component.ts
+++ prime-numbers/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IntegerNumber } from "./integer-number";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'prime-numbers';
+export class AppComponent implements OnInit {
+  primes = [2,3,5,7,11,13,17,19,23,29]
+  numbers: IntegerNumber[] = []
+
+  ngOnInit(): void {
+    this.calculateNumbers();
+  }
+
+  private calculateNumbers() {
+    // TODO: 27.12.2021 Calculate decomposiiton in a service
+    this.numbers.push(new IntegerNumber(2, [2]));
+    this.numbers.push(new IntegerNumber(3, [3]));
+    this.numbers.push(new IntegerNumber(4, [2, 2]));
+  }
 }
---
+++ prime-numbers/src/app/integer-number.ts
@@ -0,0 +1,12 @@
+export class IntegerNumber {
+  constructor(public value: number, public primeDecomposition: number[] = []) {
+  }
+
+  printPrimeDecomposition(): string {
+    if (this.primeDecomposition.length < 1) {
+      return "";
+    }
+
+    return this.primeDecomposition.map(prime => `${prime}`).join(" * ")
+  }
+}
